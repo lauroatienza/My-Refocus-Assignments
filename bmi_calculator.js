0d3444b9440b61fc26f6d876cde4ad737672f8a2
Lauro M. Atienza <d.tru.lma@gmail.com>
2022-12-04 12:30:41 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,64 +1,68 @@
-// CODE NOT MINE. FOR GIT DEMO ONLY
-
-// (A) CHANGE BMI MEASURING SYSTEM
-function measureBMI () {
-    // (A1) GET HTML ELEMENTS
-    let unit = document.getElementById("bmi-metric").checked,
-        weight = document.getElementById("bmi-weight"),
-        weightu = document.getElementById("bmi-weight-unit"),
-        height = document.getElementById("bmi-height"),
-        heightu = document.getElementById("bmi-height-unit");
    
-    // (A2) UPDATE HTML FORM FIELDS
-    // TRUE = METRIC, FALSE = IMPERIAL
-    if (unit) {
-      weightu.innerHTML = "KG";
-      weight.min = 1; weight.max = 635;
-      heightu.innerHTML = "CM";
-      height.min = 54; height.max = 272;
-    } else {
-      weightu.innerHTML = "LBS";
-      weight.min = 2; weight.max = 1400;
-      heightu.innerHTML = "IN";
-      height.min = 21; height.max = 107;
-    }
-  }
+  // CALCULATE BMI
+  function calcBMI (inputWeight,inputHeight) {
+    let bmi = 0;
+    let weight = inputWeight;
+    let height = inputHeight;
    
-  // (B) CALCULATE BMI
-  function calcBMI () {
-    // (B1) GET HTML ELEMENTS
-    let bmi = null,
-        unit = document.getElementById("bmi-metric").checked, // true = metric, false = imperial
-        weight = parseInt(document.getElementById("bmi-weight").value),
-        height = parseInt(document.getElementById("bmi-height").value),
-        results = document.getElementById("bmi-results");
+    // Formula
+    bmi = weight / (height * height);
    
-    // (B2) CALCULATE BMI
-    // METRIC BMI = MASS (KG) / HEIGHT (M) SQUARE
-    if (unit) {
-      height = height / 100;
-      bmi = weight / (height * height);
-    }
-    // IMPERIAL BMI = 703 X MASS (LBS) / HEIGHT (IN) SQUARE
-    else {
-      bmi = 703 * (weight / (height * height));
-    }
-    // ROUND OFF
+      // ROUND OFF
     bmi = Math.round(bmi * 100) / 100; // Round off 2 decimal places
    
     // (B3) SHOW RESULTS
     if (bmi < 18.5) {
-      results.innerHTML = bmi + " - Underweight";
+      console.log(bmi + " - Underweight");
+      return "Underweight";
     } else if (bmi < 25) {
-      results.innerHTML = bmi + " - Normal weight";
+      console.log(bmi + " - Normal weight");
+      return "Normal weight";
     } else if (bmi < 30) {
-      results.innerHTML = bmi + " - Pre-obesity";
+      console.log(bmi + " - Pre-obesity");
+      return "Pre-obesity";
     } else if (bmi < 35) {
-      results.innerHTML = bmi + " - Obesity class I";
+      console.log(bmi + " - Obesity class I");
+      return "Obesity class I";
     } else if (bmi < 40) {
-      results.innerHTML = bmi + " - Obesity class II";
+      console.log(bmi + " - Obesity class II");
+      return "Obesity class II";
     } else {
-      results.innerHTML = bmi + " - Obesity class III";
+      console.log(bmi + " - Obesity class III");
+      return "Obesity class III";
     }
     return false;
   }
+
+// input unit is Kilograms and Meters
+  // calcBMI(50,1.5);
+
+
+//Test function
+function testBmi(){
+    const result = calcBMI(60,1.5);
+    
+    if (result == "Underweight") {
+      const expected = "Underweight";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    } else if (result == "Normal weight") {
+      const expected = "Normal weight";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    } else if (result == "Pre-obesity") {
+      const expected = "Pre-obesity";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    } else if (result == "Obesity class I") {
+      const expected = "Obesity class I";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    } else if (result == "Obesity class II") {
+      const expected = "Obesity class II";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    } else if (result == "Obesity class I") {
+      const expected = "Obesity class III";
+      console.assert(result === expected, 'The result ' + result + ' dont match the expected value ' + expected);
+    }
+    return false;
+}
+
+testBmi();
+
